b2235950fc2f4d115db79720e85073718dd562a6
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2021-10-25 14:32:29 +0200

    Añadida alerta, inicio lección 76

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,13 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+            child: const Text('Mostrar alerta'),
+            onPressed: () => _mostrarAlerta(context),
+            style: ElevatedButton.styleFrom(
+                primary: Colors.yellow[800], shape: const StadiumBorder())),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_back),
         onPressed: () {
@@ -16,5 +23,40 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0)),
+              title: const Text('Título de la alerta'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Text('Este es el contenido del a caja de la alerta'),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  FlutterLogo(
+                    size: 100,
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancelar'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('OK'),
+                ),
+              ]);
+        },
+        barrierDismissible: true,
+        barrierColor: Colors.blue[800]);
   }
 }
