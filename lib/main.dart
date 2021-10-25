c302d288670fd11f7483d10d2829d068f047cc33
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2021-10-25 12:56:02 +0200

    inicio lección 70, añadidas rutas básicas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_componentes/src/pages/alert_page.dart';
+import 'package:flutter_componentes/src/pages/avatar_page.dart';
 import 'package:flutter_componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage(),
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomePage(),
+        'alert': (BuildContext context) => const AlertPage(),
+        'avatar': (BuildContext context) => const AvatarPage(),
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_componentes/src/pages/alert_page.dart';
+import 'package:flutter_componentes/src/providers/menu_provider.dart';
+import 'package:flutter_componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,25 +17,53 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    // menuProvider.cargarData();
+
+    /* return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
+    ); */
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data ?? [], context),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo'),
-      ),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
+
+    for (var element in data) {
+      final widgetTemp = ListTile(
+        title: Text(element['texto']),
+        leading: getIcon(element['icon']),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          // esto sería muy hardcodeado con un switch o parecidos, y no es lo que buscamos
+          /* final route =
+              MaterialPageRoute(builder: (context) => const AlertPage());
+          Navigator.push(context, route); */
+
+          // la ruta que pusheamos DEBE estar definida en main.dart
+          Navigator.pushNamed(context, element['ruta']);
+        },
+      );
+
+      opciones
+        ..add(widgetTemp)
+        ..add(const Divider());
+    }
+
+    return opciones;
   }
 }
