fb7c6ca6c41bc9c52484a45ffd83c24d32b1da30
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2021-10-26 13:40:21 +0200

    añadidos avatares

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,9 +6,33 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Avatar Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Avatar Page'),
+          actions: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(5.0),
+              child: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://ichef.bbci.co.uk/news/800/cpsprodpb/A85B/production/_104299034_tv050571340.jpg'),
+                radius: 30.0,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(right: 10.0),
+              child: CircleAvatar(
+                child: const Text('SL'),
+                backgroundColor: Colors.brown[500],
+              ),
+            )
+          ],
+        ),
+        body: const Center(
+          child: FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            image: NetworkImage(
+                'https://ichef.bbci.co.uk/news/800/cpsprodpb/A85B/production/_104299034_tv050571340.jpg'),
+          ),
+        ));
   }
 }
