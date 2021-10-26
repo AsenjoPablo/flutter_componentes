68cab0fb645285b908c0b4de36a0ca29be8a018a
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2021-10-26 14:02:16 +0200

    AnimatedContainer & curves

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_componentes/src/pages/alert_page.dart';
+import 'package:flutter_componentes/src/pages/animated_container_page.dart';
 import 'package:flutter_componentes/src/pages/avatar_page.dart';
 import 'package:flutter_componentes/src/pages/card_page.dart';
 import 'package:flutter_componentes/src/pages/home_page.dart';
@@ -11,5 +12,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
     'card': (BuildContext context) => const CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 }
